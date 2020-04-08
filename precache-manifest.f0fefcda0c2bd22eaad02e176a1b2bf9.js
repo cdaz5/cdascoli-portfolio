3908898a4ffe8da33138a0218b83bf9c335ed857
Chris D'Ascoli <chris@hiclark.com>
2020-04-08 09:34:52 -0400

    Updates

---
+++ precache-manifest.f0fefcda0c2bd22eaad02e176a1b2bf9.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9414c26bd34135f2c12f7da6628f7e",
+    "url": "/cdascoli-portfolio/index.html"
+  },
+  {
+    "revision": "580b3f2eaf1111640ec2",
+    "url": "/cdascoli-portfolio/static/js/2.b20aa08f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cdascoli-portfolio/static/js/2.b20aa08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc083b61d0f5658ac33",
+    "url": "/cdascoli-portfolio/static/js/main.c0f92988.chunk.js"
+  },
+  {
+    "revision": "9ad6f8dbdaa210baeaf5",
+    "url": "/cdascoli-portfolio/static/js/runtime-main.4b0339ef.js"
+  },
+  {
+    "revision": "3f519fd6b44c0f8d527dab8d383ed787",
+    "url": "/cdascoli-portfolio/static/media/angry-weather.3f519fd6.png"
+  },
+  {
+    "revision": "341b206a1ab20bb15080594d81ba40f6",
+    "url": "/cdascoli-portfolio/static/media/app.341b206a.svg"
+  },
+  {
+    "revision": "d25f0c44272661a88d720d65ec3ad29b",
+    "url": "/cdascoli-portfolio/static/media/clap.d25f0c44.svg"
+  },
+  {
+    "revision": "86ba9f864468b1562082090a3ed73779",
+    "url": "/cdascoli-portfolio/static/media/email.86ba9f86.svg"
+  },
+  {
+    "revision": "0a17d6b10051c5ada94692bfd3a440c5",
+    "url": "/cdascoli-portfolio/static/media/external-link.0a17d6b1.svg"
+  },
+  {
+    "revision": "e17cd04634d60839e8a0bc1ede841904",
+    "url": "/cdascoli-portfolio/static/media/github.e17cd046.svg"
+  },
+  {
+    "revision": "e7f32a3e1c538ef387b2abf5a55d5468",
+    "url": "/cdascoli-portfolio/static/media/graphql.e7f32a3e.png"
+  },
+  {
+    "revision": "0bb938cffbe52e3473ca7a5e9125d5a0",
+    "url": "/cdascoli-portfolio/static/media/headshot.0bb938cf.jpg"
+  },
+  {
+    "revision": "ddc72857186d1d43395429f0158a85cf",
+    "url": "/cdascoli-portfolio/static/media/linkedin.ddc72857.svg"
+  },
+  {
+    "revision": "b797720ca3db1c58cf59654eb0572fb8",
+    "url": "/cdascoli-portfolio/static/media/medium.b797720c.svg"
+  },
+  {
+    "revision": "b28c7874d8fe924929be62b2290c5f03",
+    "url": "/cdascoli-portfolio/static/media/moon.b28c7874.svg"
+  },
+  {
+    "revision": "be2df79601b32143d336bd076429b909",
+    "url": "/cdascoli-portfolio/static/media/nodejs.be2df796.svg"
+  },
+  {
+    "revision": "92ffef87cdddd682d513f12b3404d972",
+    "url": "/cdascoli-portfolio/static/media/open-source.92ffef87.png"
+  },
+  {
+    "revision": "8251fe76d7487a28b07603515b300964",
+    "url": "/cdascoli-portfolio/static/media/redux.8251fe76.png"
+  },
+  {
+    "revision": "154808e5d8dcae2a3671477d3a873802",
+    "url": "/cdascoli-portfolio/static/media/slacker.154808e5.png"
+  },
+  {
+    "revision": "d80810a770ff6b792d06433e55307f8c",
+    "url": "/cdascoli-portfolio/static/media/sun.d80810a7.svg"
+  }
+]);
